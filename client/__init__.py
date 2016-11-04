cc65f59eef979ac055437b5d9c44b99526dc8b73
Wind_Protocol <3dsarcard@gmail.com>
2016-11-03 21:33:35 -0500

    Delete __init__.py

--- client/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._parser import ApricotParser
-from ._request import ApricotRequest
-from ._response import ApricotResponse
-from ._protocol import ApricotProtocol
-from ._response import ApricotHttpResponse
-
-from ._client import ApricotClient
-from ._session import ApricotSession
